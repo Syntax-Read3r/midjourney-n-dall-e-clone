24c388de87f12f285f29cdf6993157c686456ba1
Munyaradzi <syntaxread3r@gmail.com>
2023-02-01 21:43:12 +0000

    work on Home page and add section > div*3

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CArd = () => {
+  return (
+    <div>CArd</div>
+  )
+}
+
+export default CArd
---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+import Card from './Card';
+import FormField from './FormField';
+import Loader from './Loader';
+
+export {
+    Loader,
+    Card,
+    FormField
+};
