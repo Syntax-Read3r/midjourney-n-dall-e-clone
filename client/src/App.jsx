65b6b12968f515379d727ad7a75d9ac9bfcd50d4
Munyaradzi <syntaxread3r@gmail.com>
2023-02-01 20:25:26 +0000

    finish creating page structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,40 @@
 import React from "react";
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+
+// import a logo
+import {logo} from './assets';
+
+// import pages inside one line
+import {Home, CreatePost} from './pages';
+
 
 const App = () => {
 	return (
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		// A browserRouter 
+			<BrowserRouter>
+			{/* w-full is an acronm for full width (This is tailwind)*/}
+				<header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+					<Link to="/">
+						<img src={logo} alt="logo" className="w-28 object-contain" />
+					</Link>
+
+					<Link to="/posts" className="font-inter font-medium bg-[#6468ff] text-white px-4 py-2
+					rounded-md">
+						Create
+					</Link>
+				</header>
+
+				<main className="sm:p-8 px-4 py-8 w-full bg-[#f9f8f3] min-h-[calc(100vh-73px)]">
+
+					{/* Render routes */}
+					<Routes>
+						{/* This route path is the destination */}
+						<Route path="/" element={<Home/>} />
+
+						<Route path="/posts" element={<CreatePost/>} />
+					</Routes>
+				</main>
+			</BrowserRouter>
 	);
 };
 
---
+++ client/src/pages/index.js
@@ -0,0 +1,12 @@
+import Home from './Home';
+import CreatePost from './CreatePost';
+
+
+export {
+    Home,
+    CreatePost
+}
+
+// This is going to allow me to import 
+// both functional components inside one 
+// line:
