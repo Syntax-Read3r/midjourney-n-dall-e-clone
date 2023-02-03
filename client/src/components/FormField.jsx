c990f88b43a8c140e6deee5797ea1964325aad31
Munyaradzi <syntaxread3r@gmail.com>
2023-02-03 13:48:57 +0000

    finish FormField component

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -20,10 +20,20 @@ const FormField = ({labelName, type, name, placeholder, value, handleChange, isS
             Surprise Me
           </button>
         )}
-
-    
-
       </div>
+
+      <input type={type}
+      id={name}
+      name={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={handleChange}
+      required
+      className="px-2 py-2 border bg-[#ECECF1] text-sm rounded-lg w-full  text-gray-900 focus:ring-[#4649ff] focus:border-[#4649ff] focus:shadow-[#4649ff] mb-1"
+
+
+
+      />
     </div>
   )
 }
