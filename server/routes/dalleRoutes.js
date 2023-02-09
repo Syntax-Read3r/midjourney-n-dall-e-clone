88c39d335a6c2a37b62281142fc07e65851470e7
Munyaradzi <syntaxread3r@gmail.com>
2023-02-09 16:33:42 +0000

    add a possibly functional dalle api post that is yet to be tested

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -16,5 +16,27 @@ router.route('/').get((req, res) => {
     res.send('Hello from DALL-E')
 });
 
+router.router('/').post(async(req,res) => {
+    try {
+        const {prompt} = req.body; //The will come from the frontend side
+
+        const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format:'b64_json',
+        })
+
+        const image = aiResponse.data.data[0].b64_json;
+
+        res.status(200).json({photo: image});
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error?.response.data.error.message || error);
+
+    }
+})
+
 export default router;
 
