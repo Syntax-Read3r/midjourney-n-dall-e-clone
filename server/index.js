64d1119d8a1dd896f8a0f78b61b6496b78770ee9
Munyaradzi <syntaxread3r@gmail.com>
2023-02-09 15:04:29 +0000

    create a database on MongoDB website and create a dotenv file and correct or add .js at the end of import connectDB since this is node and not react

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
-import connectDB from './mongodb/connect';
+import connectDB from './mongodb/connect.js';
 
 dotenv.config(); //this line allow me to pull environment variable from the dotenv file
 
